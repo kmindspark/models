26b3d4fdc37162abbf5042ba3780141d9da0ed46
Kaushik Shivakumar <kaushikshiv@google.com>
2020-07-08 07:20:19 +0000

    work more

--- research/object_detection/meta_architectures/detr_meta_arch.py
+++ research/object_detection/meta_architectures/detr_meta_arch.py
@@ -38,100 +38,44 @@ class DETRMetaArch(model.DetectionModel):
         return self.bboxes(x), self.cls(x)
 
     def loss(self, prediction_dict, true_image_shapes, scope=None):
-        
+        return 1
 
-    def postprocess(self, prediction_dict, true_image_shapes):
-    """Convert prediction tensors to final detections.
+    def preprocess(self, inputs):
+        """Feature-extractor specific preprocessing.
 
-    This function converts raw predictions tensors to final detection results.
-    See base class for output format conventions.  Note also that by default,
-    scores are to be interpreted as logits, but if a score_converter is used,
-    then scores are remapped (and may thus have a different interpretation).
+        See base class.
 
-    If number_of_stages=1, the returned results represent proposals from the
-    first stage RPN and are padded to have self.max_num_proposals for each
-    image; otherwise, the results can be interpreted as multiclass detections
-    from the full two-stage model and are padded to self._max_detections.
+        For Faster R-CNN, we perform image resizing in the base class --- each
+        class subclassing FasterRCNNMetaArch is responsible for any additional
+        preprocessing (e.g., scaling pixel values to be in [-1, 1]).
 
-    Args:
-      prediction_dict: a dictionary holding prediction tensors (see the
-        documentation for the predict method.  If number_of_stages=1, we
-        expect prediction_dict to contain `rpn_box_encodings`,
-        `rpn_objectness_predictions_with_background`, `rpn_features_to_crop`,
-        and `anchors` fields.  Otherwise we expect prediction_dict to
-        additionally contain `refined_box_encodings`,
-        `class_predictions_with_background`, `num_proposals`,
-        `proposal_boxes` and, optionally, `mask_predictions` fields.
-      true_image_shapes: int32 tensor of shape [batch, 3] where each row is
-        of the form [height, width, channels] indicating the shapes
-        of true images in the resized images, as resized images can be padded
-        with zeros.
+        Args:
+        inputs: a [batch, height_in, width_in, channels] float tensor representing
+            a batch of images with values between 0 and 255.0.
 
-    Returns:
-      detections: a dictionary containing the following fields
-        detection_boxes: [batch, max_detection, 4]
-        detection_scores: [batch, max_detections]
-        detection_multiclass_scores: [batch, max_detections, 2]
-        detection_anchor_indices: [batch, max_detections]
-        detection_classes: [batch, max_detections]
-          (this entry is only created if rpn_mode=False)
-        num_detections: [batch]
-        raw_detection_boxes: [batch, total_detections, 4]
-        raw_detection_scores: [batch, total_detections, num_classes + 1]
+        Returns:
+        preprocessed_inputs: a [batch, height_out, width_out, channels] float
+            tensor representing a batch of images.
+        true_image_shapes: int32 tensor of shape [batch, 3] where each row is
+            of the form [height, width, channels] indicating the shapes
+            of true images in the resized images, as resized images can be padded
+            with zeros.
+        Raises:
+        ValueError: if inputs tensor does not have type tf.float32
+        """
 
-    Raises:
-      ValueError: If `predict` is called before `preprocess`.
-      ValueError: If `_output_final_box_features` is true but
-        rpn_features_to_crop is not in the prediction_dict.
-    """
+        with tf.name_scope('Preprocessor'):
+            (resized_inputs,
+            true_image_shapes) = shape_utils.resize_images_and_return_shapes(
+                inputs, self._image_resizer_fn)
 
-    with tf.name_scope('FirstStagePostprocessor'):
-      if self._number_of_stages == 1:
+        return (self.feature_extractor.preprocess(resized_inputs),
+                true_image_shapes)
 
-        image_shapes = self._image_batch_shape_2d(
-            prediction_dict['image_shape'])
-        (proposal_boxes, proposal_scores, proposal_multiclass_scores,
-         num_proposals, raw_proposal_boxes,
-         raw_proposal_scores) = self._postprocess_rpn(
-             prediction_dict['rpn_box_encodings'],
-             prediction_dict['rpn_objectness_predictions_with_background'],
-             prediction_dict['anchors'], image_shapes, true_image_shapes)
-        return {
-            fields.DetectionResultFields.detection_boxes:
-                proposal_boxes,
-            fields.DetectionResultFields.detection_scores:
-                proposal_scores,
-            fields.DetectionResultFields.detection_multiclass_scores:
-                proposal_multiclass_scores,
-            fields.DetectionResultFields.num_detections:
-                tf.cast(num_proposals, dtype=tf.float32),
-            fields.DetectionResultFields.raw_detection_boxes:
-                raw_proposal_boxes,
-            fields.DetectionResultFields.raw_detection_scores:
-                raw_proposal_scores
-        }
+    def restore_from_objects(self, fine_tune_checkpoint_type='detection'):
+        raise NotImplementedError("Model restoration implemented yet.")
 
-    # TODO(jrru): Remove mask_predictions from _post_process_box_classifier.
-    if (self._number_of_stages == 2 or
-        (self._number_of_stages == 3 and self._is_training)):
-      with tf.name_scope('SecondStagePostprocessor'):
-        mask_predictions = prediction_dict.get(box_predictor.MASK_PREDICTIONS)
-        detections_dict = self._postprocess_box_classifier(
-            prediction_dict['refined_box_encodings'],
-            prediction_dict['class_predictions_with_background'],
-            prediction_dict['proposal_boxes'],
-            prediction_dict['num_proposals'],
-            true_image_shapes,
-            mask_predictions=mask_predictions)
-
-      if self._output_final_box_features:
-        if 'rpn_features_to_crop' not in prediction_dict:
-          raise ValueError(
-              'Please make sure rpn_features_to_crop is in the prediction_dict.'
-          )
-        detections_dict[
-            'detection_features'] = self._add_detection_features_output_node(
-                detections_dict[fields.DetectionResultFields.detection_boxes],
-                prediction_dict['rpn_features_to_crop'])
-
-      return detections_dict
+    def restore_map(self,
+                    fine_tune_checkpoint_type='detection',
+                    load_all_detection_checkpoint_vars=False):
+        raise NotImplementedError("Model restoration implemented yet.")
