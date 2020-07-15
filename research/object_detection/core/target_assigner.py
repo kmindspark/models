74385b1876cc6b97a169ea5dc3e7785124617046
Kaushik Shivakumar <kaushikshiv@google.com>
2020-07-15 22:41:50 +0000

    fix

--- research/object_detection/core/target_assigner.py
+++ research/object_detection/core/target_assigner.py
@@ -286,7 +286,7 @@ class TargetAssigner(object):
           ignored_value=tf.zeros(groundtruth_keypoints.get_shape()[1:]))
       matched_gt_boxlist.add_field(fields.BoxListFields.keypoints,
                                    matched_keypoints)
-    print(matched_gt_boxlist)
+    print(matched_gt_boxlist.data['boxes'])
     matched_reg_targets = self._box_coder.encode(matched_gt_boxlist, anchors)
     print(matched_reg_targets)
     match_results_shape = shape_utils.combined_static_and_dynamic_shape(
