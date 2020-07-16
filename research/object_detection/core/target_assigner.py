9081a0bdd137ebeb3124eeebe24dc2806a663174
Kaushik Shivakumar <kaushikshiv@google.com>
2020-07-16 00:26:35 +0000

    fix

--- research/object_detection/core/target_assigner.py
+++ research/object_detection/core/target_assigner.py
@@ -462,7 +462,7 @@ def create_target_assigner(reference, stage=None,
   elif reference == 'DETR':
     similarity_calc = sim_calc.IouSimilarity()
     matcher = hungarian_matcher.HungarianBipartiteMatcher()
-    box_coder_instance = hungarian_matcher.HungarianBipartiteMatcher()
+    box_coder_instance = detr_box_coder.DETRBoxCoder()
 
   else:
     raise ValueError('No valid combination of reference and stage.')
