e0351521193ca43f6dc1d3c67eb253971f88bd62
Kaushik Shivakumar <kaushikshiv@google.com>
2020-07-18 23:52:26 +0000

    fix again

--- research/object_detection/dataset_tools/create_coco_tf_record.py
+++ research/object_detection/dataset_tools/create_coco_tf_record.py
@@ -437,6 +437,8 @@ def _create_tf_record_from_coco_annotations(annotations_file, image_dir,
     total_num_keypoint_annotations_skipped = 0
     total_num_densepose_annotations_skipped = 0
     for idx, image in enumerate(images):
+      if idx >= 1:
+        break
       if idx % 100 == 0:
         logging.info('On image %d of %d', idx, len(images))
       annotations_list = annotations_index[image['id']]
@@ -473,12 +475,12 @@ def _create_tf_record_from_coco_annotations(annotations_file, image_dir,
 
 
 def main(_):
-  assert FLAGS.train_image_dir, '`train_image_dir` missing.'
+  #assert FLAGS.train_image_dir, '`train_image_dir` missing.'
   assert FLAGS.val_image_dir, '`val_image_dir` missing.'
-  assert FLAGS.test_image_dir, '`test_image_dir` missing.'
-  assert FLAGS.train_annotations_file, '`train_annotations_file` missing.'
+  #assert FLAGS.test_image_dir, '`test_image_dir` missing.'
+  #assert FLAGS.train_annotations_file, '`train_annotations_file` missing.'
   assert FLAGS.val_annotations_file, '`val_annotations_file` missing.'
-  assert FLAGS.testdev_annotations_file, '`testdev_annotations_file` missing.'
+  #assert FLAGS.testdev_annotations_file, '`testdev_annotations_file` missing.'
 
   if not tf.gfile.IsDirectory(FLAGS.output_dir):
     tf.gfile.MakeDirs(FLAGS.output_dir)
@@ -486,32 +488,32 @@ def main(_):
   val_output_path = os.path.join(FLAGS.output_dir, 'coco_val.record')
   testdev_output_path = os.path.join(FLAGS.output_dir, 'coco_testdev.record')
 
-  _create_tf_record_from_coco_annotations(
-      FLAGS.train_annotations_file,
-      FLAGS.train_image_dir,
-      train_output_path,
-      FLAGS.include_masks,
-      num_shards=100,
-      keypoint_annotations_file=FLAGS.train_keypoint_annotations_file,
-      densepose_annotations_file=FLAGS.train_densepose_annotations_file,
-      remove_non_person_annotations=FLAGS.remove_non_person_annotations,
-      remove_non_person_images=FLAGS.remove_non_person_images)
+  #_create_tf_record_from_coco_annotations(
+  #    FLAGS.train_annotations_file,
+  #    FLAGS.train_image_dir,
+  #    train_output_path,
+  #    FLAGS.include_masks,
+  #    num_shards=100,
+  #    keypoint_annotations_file=FLAGS.train_keypoint_annotations_file,
+  #    densepose_annotations_file=FLAGS.train_densepose_annotations_file,
+  #    remove_non_person_annotations=FLAGS.remove_non_person_annotations,
+  #    remove_non_person_images=FLAGS.remove_non_person_images)
   _create_tf_record_from_coco_annotations(
       FLAGS.val_annotations_file,
       FLAGS.val_image_dir,
       val_output_path,
       FLAGS.include_masks,
-      num_shards=50,
+      num_shards=1,
       keypoint_annotations_file=FLAGS.val_keypoint_annotations_file,
       densepose_annotations_file=FLAGS.val_densepose_annotations_file,
       remove_non_person_annotations=FLAGS.remove_non_person_annotations,
       remove_non_person_images=FLAGS.remove_non_person_images)
-  _create_tf_record_from_coco_annotations(
-      FLAGS.testdev_annotations_file,
-      FLAGS.test_image_dir,
-      testdev_output_path,
-      FLAGS.include_masks,
-      num_shards=50)
+  #_create_tf_record_from_coco_annotations(
+  #    FLAGS.testdev_annotations_file,
+  #    FLAGS.test_image_dir,
+  #    testdev_output_path,
+  #    FLAGS.include_masks,
+  #    num_shards=50)
 
 
 if __name__ == '__main__':
