6f6ea40f986f41b65f40f9fc216dd10d7c4d7b33
Kaushik Shivakumar <kaushikshiv@google.com>
2020-07-19 20:57:02 +0000

    fix again

--- research/object_detection/meta_architectures/detr_meta_arch.py
+++ research/object_detection/meta_architectures/detr_meta_arch.py
@@ -108,7 +108,7 @@ class DETRMetaArch(model.DetectionModel):
     x = self.transformer([x, tf.repeat(tf.expand_dims(self.queries, 0), x.shape[0], axis=0)])
     bboxes_encoded, logits = self._box_ffn(x), self.cls_activation(self.cls(x))
 
-    fake_logits = np.zeros((1, 100, 91))
+    fake_logits = np.zeros((1, 10, 91))
     fake_logits[:,:,5] = 1
     logits = tf.convert_to_tensor(fake_logits, dtype=tf.float32)
     #print(logits)
