ce74e3b1733077c7985cbbc20963b654d42ebfbe
Kaushik Shivakumar <kaushikshiv@google.com>
2020-07-20 04:46:13 +0000

    fix

--- research/object_detection/meta_architectures/detr_meta_arch.py
+++ research/object_detection/meta_architectures/detr_meta_arch.py
@@ -385,10 +385,10 @@ class DETRMetaArch(model.DetectionModel):
 
       def convert_to_minmaxcoords(input_tensor):
         reshaped_encodings = tf.reshape(input_tensor, [-1, 4])
-        ycenter = tf.gather(reshaped_encodings, [0])
-        xcenter = tf.gather(reshaped_encodings, [1])
-        h = tf.gather(reshaped_encodings, [:, 2])
-        w = tf.gather(reshaped_encodings, [:, 3])
+        ycenter = tf.gather(reshaped_encodings, [0], axis=1)
+        xcenter = tf.gather(reshaped_encodings, [1], axis=1)
+        h = tf.gather(reshaped_encodings, [2], axis=1)
+        w = tf.gather(reshaped_encodings, [3], axis=1)
         ymin = ycenter - h / 2.
         xmin = xcenter - w / 2.
         ymax = ycenter + h / 2.
