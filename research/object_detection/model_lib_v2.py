9c2dd35520adb0d8b35af83b61f421fe2e37402e
Kaushik Shivakumar <kaushikshiv@google.com>
2020-07-22 19:16:48 +0000

    fix

--- research/object_detection/model_lib_v2.py
+++ research/object_detection/model_lib_v2.py
@@ -261,6 +261,9 @@ def eager_train_step(detection_model,
   gradients = tape.gradient(total_loss, trainable_variables)
 
   print("TYPE: ", type(trainable_variables))
+  new_list = []
+  for i in range(len(trainable_variables)):
+    new_list.append(trainable_variables[i].name)
   for i in range(len(trainable_variables)):
     if (trainable_variables[i].name.startswith("FirstStage")):
       print("Reducing gradient")
