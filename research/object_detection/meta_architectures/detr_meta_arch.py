29df24be026975b2b323d3098650574c98c6f4b3
Kaushik Shivakumar <kaushikshiv@google.com>
2020-08-02 05:21:12 +0000

    detr

--- research/object_detection/meta_architectures/detr_meta_arch.py
+++ research/object_detection/meta_architectures/detr_meta_arch.py
@@ -121,9 +121,10 @@ class DETRMetaArch(model.DetectionModel):
     print("Actually predicted logits: ", logits)
     print("Queries", self.queries)
 
-    #fake_logits = np.zeros((x.shape[0], 100, self.num_classes + 1))
-    #fake_logits[:,:,1] = 1000
-    #logits = tf.convert_to_tensor(fake_logits, dtype=tf.float32)
+    if (not self.is_training):
+      fake_logits = np.zeros((x.shape[0], 100, self.num_classes + 1))
+      fake_logits[:,:,1] = 1000
+      logits = tf.convert_to_tensor(fake_logits, dtype=tf.float32)
 
     print("Predicted logits")
     #print(bboxes_encoded)
