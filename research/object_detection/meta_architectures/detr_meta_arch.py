d4c3179895fa758597cd0e3c9dc77adf95e89f98
Kaushik Shivakumar <kaushikshiv@google.com>
2020-08-03 00:32:31 +0000

    attempt to revert to somewhat working

--- research/object_detection/meta_architectures/detr_meta_arch.py
+++ research/object_detection/meta_architectures/detr_meta_arch.py
@@ -75,11 +75,12 @@ class DETRMetaArch(model.DetectionModel):
     #for layer in self.first_stage.layers:
     #  layer.trainable = False
     self.target_assigner = target_assigner.create_target_assigner('DETR', 'detection')
-    self.transformer_args = {"hidden_size": self.hidden_dimension, "attention_dropout": 0.0, "num_heads": 8, "layer_postprocess_dropout": 0.0, "dtype": tf.float32, 
-      "num_hidden_layers": 3, "filter_size": 256, "relu_dropout": 0.0}
-    self.transformer = detr_lib.Transformer(attention_dropout=0.0, layer_postprocess_dropout=0.0,
-    relu_dropout=0.0, hidden_size=self.hidden_dimension, filter_size=self.hidden_dimension,
-    num_hidden_layers=3)#self.transformer_args) #hidden_size=self.hidden_dimension, filter_size=self.hidden_dimension)
+    #self.transformer = #detr_lib.Transformer(attention_dropout=0.0, layer_postprocess_dropout=0.0,
+    #relu_dropout=0.0, hidden_size=self.hidden_dimension, filter_size=self.hidden_dimension,
+    #num_hidden_layers=3)#self.transformer_args) #hidden_size=self.hidden_dimension, filter_size=self.hidden_dimension)
+    self.transformer_args = {"hidden_size": self.hidden_dimension, "attention_dropout": 0, "num_heads": 8, "layer_postprocess_dropout": 0, "dtype": tf.float32, 
+      "num_hidden_layers": 4, "filter_size": 128, "relu_dropout": 0}
+    self.transformer = detr_transformer.Transformer(self.transformer_args)
     #self.ffn = self.feature_extractor.get_box_classifier_feature_extractor_model()
     #self.bboxes = tf.keras.layers.Dense(4)
     self.cls = tf.keras.layers.Dense(num_classes + 1)
