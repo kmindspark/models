6301163574580e297c1af7e9457003a48044fad8
Kaushik Shivakumar <kaushikshiv@google.com>
2020-08-06 01:29:57 +0000

    try fixing main model

--- research/object_detection/meta_architectures/detr_lib.py
+++ research/object_detection/meta_architectures/detr_lib.py
@@ -190,6 +190,48 @@ class Transformer(tf.keras.Model):
 class PrePostProcessingWrapper(tf.keras.layers.Layer):
   """Wrapper class that applies layer pre-processing and post-processing."""
 
+  def __init__(self, layer, params):
+    super(PrePostProcessingWrapper, self).__init__()
+    self.layer = layer
+    self.params = params
+    self.postprocess_dropout = params["layer_postprocess_dropout"]
+
+  def build(self, input_shape):
+    # Create normalization layer
+    self.layer_norm = tf.keras.layers.LayerNormalization(
+        epsilon=1e-6, dtype="float32")
+    self.layer_pre_norm = tf.keras.layers.LayerNormalization(
+        epsilon=1e-6, dtype="float32")
+    super(PrePostProcessingWrapper, self).build(input_shape)
+
+  def get_config(self):
+    return {
+        "params": self.params,
+    }
+
+  def call(self, add, x, *args, **kwargs):
+    """Calls wrapped layer with same parameters."""
+    # Preprocessing: apply layer normalization
+    training = kwargs["training"]
+
+    y = self.layer_pre_norm(x)
+    newargs = [y]
+    if len(args) == 1:
+      newargs.append(self.layer_pre_norm(args[0]))
+    else:
+      newargs.extend(args)
+    
+    # Get layer output
+    y = self.layer(*newargs, **kwargs)
+
+    # Postprocessing: apply dropout and residual connection
+    if training:
+      y = tf.nn.dropout(y, rate=self.postprocess_dropout)
+    return add + y
+
+class PrePostProcessingWrapperOld(tf.keras.layers.Layer):
+  """Wrapper class that applies layer pre-processing and post-processing."""
+
   def __init__(self, layer, layer_postprocess_dropout):
     super(PrePostProcessingWrapper, self).__init__()
     self.layer = layer
